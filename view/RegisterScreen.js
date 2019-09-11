4a667f1276e6d44ef68db66049f156e49d0a1fd2
Jhon Sebastian Garcia Izquierdo <1005976753@academico.icesi.edu.co>
2019-09-10 19:15:08 -0500

    cambios menores

--- view/RegisterScreen.js
+++ view/RegisterScreen.js
@@ -1,80 +1,80 @@
 class RegisterScreen {
 	
-	fullName = "";
-	email = "";
-	password = "";
-	confirmedPassword = "";
-	arrayInputs = [];
+	 fullName = "";
+	 email = "";
+	 password = "";
+	 confirmedPassword = "";
+	 arrayInputs = [];
 
-	constructor() {
+	 constructor() {
+ 
+		var incrementPosY = 0;
 
-	   var incrementPosY = 0;
+		for (var i = 0; i < 4; i++) {
+			if(i == i){
+				this.arrayInputs[i]  = new Input(200, 250+incrementPosY, "text");
+			}
+			incrementPosY+=50;
+		}
+	}
+	
+	paint() {
+		for (var i = 0; i < this.arrayInputs.length; i++) {
+			this.arrayInputs[i].paint();
+		}
+		
+		fill(0);
+		rect(200,550,200,30);
+		fill(255);
+		text("Crear Usuario",200+50,550+15);
 
-	   for (var i = 0; i < 4; i++) {
-		   if(i == i){
-			   this.arrayInputs[i]  = new Input(200, 250+incrementPosY, "text");
-		   }
-		   incrementPosY+=50;
-	   }
-   }
-   
-   paint() {
-	   for (var i = 0; i < this.arrayInputs.length; i++) {
-		   this.arrayInputs[i].paint();
-	   }
-	   
-	   fill(0);
-	   rect(200,550,200,30);
-	   fill(255);
-	   text("Crear Usuario",200+50,550+15);
+		console.log(this.password===this.confirmedPassword);
+	}
 
-	   console.log(this.password===this.confirmedPassword);
-   }
+	 focusInputs( mouseX,  mouseY) {
+		for (var i = 0; i < this.arrayInputs.length; i++) {
+			if (mouseX >= this.arrayInputs[i].getPosX() && mouseX <= this.arrayInputs[i].getPosX() + 200
+					&& mouseY >= this.arrayInputs[i].getPosY() && mouseY <= this.arrayInputs[i].getPosY() + 30) {
+				this.arrayInputs[i].setFocus(true);
+			} else {
+				this.arrayInputs[i].setFocus(false);
+			}
+		}
+	}
 
-	focusInputs( mouseX,  mouseY) {
-	   for (var i = 0; i < this.arrayInputs.length; i++) {
-		   if (mouseX >= this.arrayInputs[i].getPosX() && mouseX <= this.arrayInputs[i].getPosX() + 200
-				   && mouseY >= this.arrayInputs[i].getPosY() && mouseY <= this.arrayInputs[i].getPosY() + 30) {
-			   this.arrayInputs[i].setFocus(true);
-		   } else {
-			   this.arrayInputs[i].setFocus(false);
-		   }
-	   }
-   }
+	  writeTextInput( key) {
+		for (var i = 0; i < this.arrayInputs.length; i++) {
+			if (this.arrayInputs[i].isFocus()) {
+				this.arrayInputs[i].setText(this.arrayInputs[i].getText() + key);
+			}
+		}
 
-	 writeTextInput( key) {
-	   for (var i = 0; i < this.arrayInputs.length; i++) {
-		   if (this.arrayInputs[i].isFocus()) {
-			   this.arrayInputs[i].setText(this.arrayInputs[i].getText() + key);
-		   }
-	   }
+		this.fullName= this.arrayInputs[0].getText();
+		this.email= this.arrayInputs[1].getText();
+		this.password= this.arrayInputs[2].getText();
+		this.confirmedPassword= this.arrayInputs[3].getText();   
+		
+	}
 
-	   this.fullName= this.arrayInputs[0].getText();
-	   this.email= this.arrayInputs[1].getText();
-	   this.password= this.arrayInputs[2].getText();
-	   this.confirmedPassword= this.arrayInputs[3].getText();   
-	   
-   }
-
-	eraseTextInput() {
-	   
-	   for (var i = 0; i < this.arrayInputs.length; i++) {
-		   if (this.arrayInputs[i].isFocus() ) {
-			   var indice = this.arrayInputs[i].getText().length - 1;
-			   this.arrayInputs[i].setText(this.arrayInputs[i].getText().substring(0, indice));
-		   }
-	   }
-	   
-   }
-   
-	confirmedP(password,confirmedPassword ) {
-	   if(password===confirmedPassword) {
-		   return true;
-	   } else {
-	   return false;}
-	   
-	   
-	   
-   }
+	 eraseTextInput() {
+		
+		for (var i = 0; i < this.arrayInputs.length; i++) {
+			if (this.arrayInputs[i].isFocus() ) {
+				var indice = this.arrayInputs[i].getText().length - 1;
+				this.arrayInputs[i].setText(this.arrayInputs[i].getText().substring(0, indice));
+			}
+		}
+		
+	}
+	
+	 confirmedP(password,confirmedPassword ) {
+		if(password===confirmedPassword) {
+			return true;
+		} else {
+		return false;}
+		
+		
+		
+	}
 
 }
