35427c96ce68ac942228d589966a557e64210308
Nicolas Montero <nicolasmontero42@gmail.com>
2019-09-13 11:22:49 -0500

    cambios mínimos
    necesarios pero no significativos

--- view/Platos.js
+++ view/Platos.js
@@ -1,9 +1,13 @@
 class Platos{
 
-constructor(){
+    nombre;
+    descripcion;
+    precio;
 
-
-
+constructor(nombre,descripcion,precio){
+    this.nombre = nombre;
+    this.descripcion = descripcion;
+    this.precio = precio;
 }
 
 }
