90b8a321f433434f9e866eb09f347d549be6e24d
Nicolas Montero <nicolasmontero42@gmail.com>
2019-09-13 11:25:57 -0500

    cambios mínimos
    cambios necesarios pero no significativos

--- view/Pedidos.js
+++ view/Pedidos.js
@@ -1,7 +1,18 @@
 class Pedidos{
 
+    pedidos = [];
+    lastIndex;
 
+    constructor (){
+        this.lastIndex = 0;
+    }
 
+    adicionarPlatoPedido(plato){
+        if(lastIndex != 0){
+            this.lastIndex = this.lastIndex + 1;
+        }
+        
+        pedidos[this.lastIndex] = plato;
+    }
 
-    
 }
